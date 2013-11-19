d8ccdd328fed6da35e92a5d06fb30798e55d8872
edward sharp <edward@edwardsharp.net>
2013-11-18 22:34:20 -0500

    puppet fixup
    Signed-off-by: edward sharp <edward@edwardsharp.net>

---
+++ manifests/site.pp
@@ -0,0 +1,28 @@
+#puppet essential... 
+group { 'puppet': ensure => 'present' }
+
+#global path def.
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] } 
+
+node default {
+    include rvm
+    include users
+
+    exec { "update_package_list":
+      user    => "root",
+      command => "/usr/bin/apt-get update",
+    }
+
+    package {
+      [ "ntp", "git-core" ] :
+        ensure => present
+    }
+
+    rvm::install {
+      #for user
+      'faye' : ;
+    }
+
+  }
+}
+  
--- modules/rvm/manifests/init.pp
+++ modules/rvm/manifests/init.pp
@@ -1,38 +1,3 @@
-#puppet essential... 
-group { 'puppet': ensure => 'present' }
-
-#global path def.
-Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] } 
-
-class app {
-
-  exec { "update_package_list":
-    user    => "root",
-    command => "/usr/bin/apt-get update",
-  }
-
-  package {
-    [ "ntp", "git-core" ] :
-      ensure => present
-  }
-
-  rvm::install {
-    #for user
-    'faye' : ;
-  }
-
-}
-
-class users {
-
-  user { "faye":
-    ensure         => "present",
-    shell         => "/bin/bash",
-    managehome         => true,
-  }
-
-}
-
 class rvm {
   # Requirements needed for building rubies and friends
   package {
